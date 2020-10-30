dafeaca5fd9a3b2022026685c7b5532013336ee4
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-10-30 15:32:40 +0530

    added open_source page and linked pages in navbar

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index_handler, name='index'),
     path('internship/', views.internship_handler, name="internship"),
+    path('open_source/', views.open_source_handler, name='open_source'),
 ]
