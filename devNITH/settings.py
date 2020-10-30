66c0603c99c0546e9a724e709f1373269d4844bd
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-10-31 02:23:41 +0530

    allowed all hosts

--- devNITH/settings.py
+++ devNITH/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config.CONFIG_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
