c7ded72708d7526905bbe9c3f15f242ef7cd4fb4
shwetalsoni <sonishwetal704@gmail.com>
2020-10-31 15:02:57 +0530

    fixed a bug

--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,10 @@ from .models import Account
 
 class UserCreateForm(UserCreationForm):
 	class Meta:
-        model = Account
-		fields = ('username', 'email', 'password1', 'password2','organisation')
+       
+		fields = ('username', 'email', 'password1', 'password2')
 		model = get_user_model()
-        # model = Account
-
+      
 		def __init__(self, *args, **kwargs):
 			super().__init__(*args, **kwargs)
 			self.fields['username'].label.widget.attrs.update({'class': 'form-control'})
