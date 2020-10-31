c98c180f9e354b3121fb81ab4906ffef39162142
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-10-31 14:59:27 +0530

    added internship and open source description pages

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index_handler, name='index'),
     path('internship/', views.internship_handler, name="internship"),
+    path('internship/<int:id>', views.internship_description_handler, name="internship_desctiption"),
     path('open_source/', views.open_source_handler, name='open_source'),
+    path('open_source/<int:id>', views.open_source_description_handler, name="open_source_description"),
     path('register/', views.register_handler, name='register'),
 ]
--- app/views.py
+++ app/views.py
@@ -7,8 +7,20 @@ def index_handler(request):
 def internship_handler(request):
     return render(request, 'internship.html')
 
+def internship_description_handler(request, id):
+    context = {
+        "id" : id
+    }
+    return render(request, 'internship_description.html', context)
+
 def open_source_handler(request):
     return render(request, 'open_source.html')
+
+def open_source_description_handler(request, id):
+    context = {
+        "id" : id
+    }
+    return render(request, 'open_source_description.html', context)
 
 def register_handler(request):
     return render(request, 'register.html')
