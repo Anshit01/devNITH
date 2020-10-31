cec3b3ace2955a128f15c4bf1bf5c4fdbe2303d9
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-10-31 17:13:08 +0530

    fixed register page

--- app/urls.py
+++ app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('internship/<int:id>', views.internship_description_handler, name="internship_desctiption"),
     path('open_source/', views.open_source_handler, name='open_source'),
     path('open_source/<int:id>', views.open_source_description_handler, name="open_source_description"),
-    path('register/', views.register_handler, name='register'),
+    # path('register/', views.register_handler, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.Register.as_view(), name='signup'),
