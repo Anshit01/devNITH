913e1dde9af38002000471cdeefcd383288350cf
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-10-31 12:52:19 +0530

    created internship and opensource post models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
@@ -9,3 +10,21 @@ class Account(models.Model):
     password = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     address = models.CharField(max_length=1000, blank=True)
+
+class InternshipPost(models.Model):
+    recruiter = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    posted_on = models.DateField()
+    last_date = models.DateField()
+    tags = ArrayField(models.CharField(max_length=50), blank=True)
+    description = models.TextField()
+    registration_link = models.CharField(max_length=1000)
+
+class OpenSourcePost(models.Model):
+    maintainer = models.ForeignKey(Account, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    posted_on = models.DateField()
+    last_date = models.DateField()
+    tags = ArrayField(models.CharField(max_length=50), blank=True)
+    description = models.TextField()
+    registration_link = models.CharField(max_length=1000)
